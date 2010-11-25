95942de8d3c3e5cd0ff1cb659fdd34bd04215c23
Brendan Mc <admin@bren2010.com>
2010-11-25 08:56:35 -0500

    Added a bit of presentation to show off load times.

--- application/layouts/main.php
+++ application/layouts/main.php
@@ -3,6 +3,8 @@
   <title>Blue Elephant</title>
  </head>
  <body>
-  <?php echo $page_content; ?>
+  <?php echo $page_content; ?><br />
+  <br />
+  Page loaded in <?php echo round($GLOBALS['globalPageTimer']->__toString(), 3); ?> seconds.
  </body>
 </html>
