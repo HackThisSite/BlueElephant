2dbafb24d304b4c528c023d2df035c7814cdf93d
Joseph Moniz <joseph.moniz@gmail.com>
2011-11-27 22:09:40 -0800

    Refactored the sys dispatcher with an updated auto-loader and swapped out the old hook handler for the new observer system.

--- sys/dispatcher.php
+++ sys/dispatcher.php
@@ -1,127 +1,232 @@
-<?php 
-/**
-Copyright (c) 2010, HackThisSite.org
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the HackThisSite.org nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND ANY
-EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
+<?php
 /**
 * Authors:
 *   Thetan ( Joseph Moniz )
 **/
 
-$maind = getcwd().'/../';
-
-set_include_path(get_include_path() . PATH_SEPARATOR . $maind.'library');
+// add our library path to the include path
+set_include_path(
+    get_include_path() .
+    PATH_SEPARATOR .
+    dirname(dirname(__FILE__)) .
+    'library'
+);
 
 class lazyLoader
 {
+    const PREFIX            = "lazyLoaderStat:";
+    const PREFIX_MODEL      = "model:";
+    const PREFIX_CONTROLLER = "controller:";
+    const PREFIX_LIBRARY    = "library:";
+    const PREFIX_EVENT      = "events:";
+    const PREFIX_DRIVER     = "drivers:";
+
+    private $root;
+
     private static $instance;
 
-    private function __construct($hooks = false)
+    private function __construct()
     {
+        $this->root = dirname(dirname(__FILE__)) . '/';
+
         spl_autoload_register(null, false);
         spl_autoload_extensions('.php');
-        spl_autoload_register(array($this, 'model'));
-        // view loading is handled automatically by the view class
-        spl_autoload_register(array($this, 'controller'));
+        spl_autoload_register(array($this, 'cached'));
         spl_autoload_register(array($this, 'library'));
-        spl_autoload_register(array($this, 'hook'));
+        spl_autoload_register(array($this, 'model'));
+        spl_autoload_register(array($this, 'event'));
+        spl_autoload_register(array($this, 'controller'));
         spl_autoload_register(array($this, 'driver'));
     }
-    
+
+    public function cached($name)
+    {
+        $cached = apc_fetch(self::PREFIX . $name);
+
+        if ($cached === null || $cached === false)
+        {
+            return false;
+        }
+
+        include $cached;
+        return true;
+    }
+
     public function model($name)
     {
+        $key    = self::PREFIX . self::PREFIX_MODEL . $name;
+        $cached = apc_fetch($key);
+
+        if ($cached === null) { return false; }
+        if ($cached !== false)
+        {
+            apc_store(self::PREFIX . $name, $cached);
+            include $cached;
+            return true;
+        }
+
         if ($name[0] == strtoupper($name[0]))
+        {
+            apc_store(self::PREFIX . $name, null);
+            apc_store($key, null);
             return false;
-        
-        $main = $GLOBALS['maind'];
-        $file = "{$main}application/models/{$name}.php";
+        }
+
+        $name = strtolower($name);
+        $file = "{$this->root}application/models/{$name}.php";
         if (!file_exists($file))
+        {
+            apc_store(self::PREFIX . $name, null);
+            apc_store($key, null);
             return false;
-        
+        }
+
+        apc_store(self::PREFIX . $name, $file);
+        apc_store($key, $file);
         include $file;
     }
-    
+
     public function controller($name)
     {
-        if (substr($name, -11) != '_controller')
+        $key    = self::PREFIX . self::PREFIX_CONTROLLER . $name;
+        $cached = apc_fetch($key);
+
+        if ($cached === null) { return false; }
+        if ($cached !== false)
+        {
+            apc_store(self::PREFIX . $name, $cached);
+            include $cached;
+            return true;
+        }
+
+        if (strncmp($name, "controller_", 11) !== 0)
+        {
+            apc_store(self::PREFIX . $name, null);
+            apc_store($key, null);
             return false;
-        
-        $main = $GLOBALS['maind'];
-        $name = substr($name, 0, -11);
-        $file = "{$main}application/controllers/{$name}.php";
+        }
+
+        $name = substr($name, 11);
+        $file = "{$this->root}application/controllers/{$name}.php";
         if (!file_exists($file))
+        {
+            apc_store(self::PREFIX . $name, null);
+            apc_store($key, null);
             return false;
-        
+        }
+
+        apc_store(self::PREFIX . $name, $file);
+        apc_store($key, $file);
         include $file;
     }
-    
+
     public function library($name)
     {
+        $key    = self::PREFIX . self::PREFIX_LIBRARY . $name;
+        $cached = apc_fetch($key);
+
+        if ($cached === null) { return false; }
+        if ($cached !== false)
+        {
+            apc_store(self::PREFIX . $name, $cached);
+            include $cached;
+            return true;
+        }
+
         if ($name[0] != strtoupper($name[0]))
+        {
+            apc_store(self::PREFIX . $name, null);
+            apc_store($key, null);
             return false;
-        
+        }
+
         $name = strtolower($name);
-        $main = $GLOBALS['maind'];
-        $file = "{$main}library/{$name}.php";
+        $file = "{$this->root}library/{$name}.php";
         if (!file_exists($file))
+        {
+            apc_store(self::PREFIX . $name, null);
+            apc_store($key, null);
             return false;
-            
+        }
+
+        apc_store(self::PREFIX . $name, $file);
+        apc_store($key, $file);
         include $file;
     }
-    
-    public function hook($name)
+
+    public function event($name)
     {
-        if (substr($name, -5) != '_hook')
+        $key    = self::PREFIX . self::PREFIX_EVENT . $name;
+        $cached = apc_fetch($key);
+
+        if ($cached === null) {
             return false;
-        
-        $main = $GLOBALS['maind'];
-        $name = substr($name, 0, -5);
-        $file = "{$main}application/hooks/{$name}.php";
+        }
+        if ($cached !== false)
+        {
+            apc_store(self::PREFIX . $name, $cached);
+            include $cached;
+            return true;
+        }
+
+        if (strncmp($name, 'events_', 7) !== 0)
+        {
+            apc_store(self::PREFIX . $name, null);
+            apc_store($key, null);
+            return false;
+        }
+
+        $name = str_replace("_", "/", $name);
+        $file = "{$this->root}application/{$name}.php";
         if (!file_exists($file))
+        {
+            apc_store(self::PREFIX . $name, null);
+            apc_store($key, null);
             return false;
-        
+        }
+
+        apc_store(self::PREFIX . $name, $file);
+        apc_store($key, $file);
         include $file;
     }
-    
+
     public function driver($name)
     {
-        if (substr($name, -7) != '_driver')
+        $key    = self::PREFIX . self::PREFIX_DRIVER . $name;
+        $cached = apc_fetch($key);
+
+        if ($cached === null) {
             return false;
-            
-        list($name, $type) = explode('_', $name);
-        
-        $main = $GLOBALS['maind'];
-        $file = "{$main}drivers/{$type}/{$name}.php";
+        }
+        if ($cached !== false)
+        {
+            apc_store(self::PREFIX . $name, $cached);
+            include $cached;
+            return true;
+        }
+
+        if (strncmp($name, "driver_", 7) !== 0)
+        {
+            apc_store(self::PREFIX . $name, null);
+            apc_store($key, null);
+            return false;
+        }
+
+        $name = substr($name, 7, -5);
+        $file = "{$this->root}drivers/{$name}.php";
         if (!file_exists($file))
+        {
+            apc_store(self::PREFIX . $name, null);
+            apc_store($key, null);
             return false;
-        
+        }
+
+        apc_store(self::PREFIX . $name, $file);
+        apc_store($key, $file);
         include $file;
     }
-    
-    public static function initialize($hooks = false) 
+
+    public static function initialize($hooks = false)
     {
         if (!isset(self::$instance))
         {
@@ -130,7 +235,7 @@ class lazyLoader
         }
         return self::$instance;
     }
-    
+
     public function __clone()
     {
         die('Error: Can not be cloned.');
@@ -139,25 +244,21 @@ class lazyLoader
 
 lazyLoader::initialize();
 
-$hooks = HookHandler::singleton(
+$observer = Observer::singleton(
     array(
-        'ini' => array(
-            'config',
+        'request/received' => array(
             'timer',
             'layout',
             'dispatch'
         ),
-        'end' => array(
-            'layout_parse',
+        'request/ended' => array(
+            'timer',
+            'layout'
         )
     )
 );
 
-// proccess request string
-if (!$_GET['r']) $_GET['r'] = 'index/index';
-$request = explode('/', $_GET['r']);
-$controller = array_shift($request);
 
-dispatch($controller, $request, false, true);
+$observer->trigger("request/received");
 
-$hooks->runHooks('end');
+$observer->trigger("request/ended");
